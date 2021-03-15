cc5bb3e26d5b6726b57b1f972de717d68bfc6f04
SaniLubary <santiago.lp.cop@gmail.com>
2021-03-14 21:56:45 -0300

    :building_construction: im now following the react tutorial, so here's the basic project re-organized hehE'

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import Game from './Game';
 import logo from './logo.svg';
-import './App.css';
+import '../css/App.css';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Game />
         <a
           className="App-link"
           href="https://reactjs.org"
