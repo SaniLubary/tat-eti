85afd329652ace82e477c0db966a1fc155be7fd7
SaniLubary <santiago.lp.cop@gmail.com>
2021-03-27 17:51:28 -0300

    :label:

--- src/component/Game.tsx
+++ src/component/Game.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import Board from './Board';
 
 export class Game extends React.Component {
-  render() {
+  render():ReactElement {
     return (
       <div className="game">
         <div className="game-board">
