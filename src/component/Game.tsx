5605ed4e763e5dbe4febcbc2c178c5035670582f
SaniLubary <santiago.lp.cop@gmail.com>
2021-04-24 13:03:34 -0300

    WIP game history, lifting up states/functions

--- src/component/Game.tsx
+++ src/component/Game.tsx
@@ -1,20 +1,28 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import PropTypes, { InferProps } from "prop-types";
 import Board from './Board';
 
 Game.propTypes = {
-  history: PropTypes.arrayOf(PropTypes.oneOfType([
-    PropTypes.string.isRequired,
-    PropTypes.oneOf([null]).isRequired
-  ])).isRequired,
+  history: PropTypes.arrayOf(
+    PropTypes.arrayOf(PropTypes.oneOfType([
+      PropTypes.string.isRequired,
+      PropTypes.oneOf([null]).isRequired
+    ])
+  )).isRequired,
+
+  crossTurn: PropTypes.bool.isRequired,
 }
 
 Game.defaultProps = {
-  history: Array(9).fill(null)
+  history: Array(9).fill(null),
+  crossTurn: true
 }
 
-function Game({history}: InferProps<typeof Game.propTypes> ): ReactElement
+function Game({history, crossTurn}: InferProps<typeof Game.propTypes> ): ReactElement
 {
+  const [state_history, setHistory] = useState(history),
+        [state_crossTurn, setCrossTurn] = useState(crossTurn);
+  
   return (
     <div className="game">
         <div className="game-board">
