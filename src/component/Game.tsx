3a3b52e7a96c2f343b7e117bb3f6ab67cccb5cf3
SaniLubary <santiago.lp.cop@gmail.com>
2021-04-24 11:35:48 -0300

    game class to functional component

--- src/component/Game.tsx
+++ src/component/Game.tsx
@@ -1,10 +1,22 @@
 import React, { ReactElement } from 'react';
+import PropTypes, { InferProps } from "prop-types";
 import Board from './Board';
 
-export class Game extends React.Component {
-  render():ReactElement {
-    return (
-      <div className="game">
+Game.propTypes = {
+  history: PropTypes.arrayOf(PropTypes.oneOfType([
+    PropTypes.string.isRequired,
+    PropTypes.oneOf([null]).isRequired
+  ])).isRequired,
+}
+
+Game.defaultProps = {
+  history: Array(9).fill(null)
+}
+
+function Game({history}: InferProps<typeof Game.propTypes> ): ReactElement
+{
+  return (
+    <div className="game">
         <div className="game-board">
           <Board />
         </div>
@@ -13,8 +25,7 @@ export class Game extends React.Component {
           <ol>{/* TODO */}</ol>
         </div>
       </div>
-    );
-  }
+  )
 }
 
 export default Game;
