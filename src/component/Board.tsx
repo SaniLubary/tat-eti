50f09bd669560590c4c645c1d3eaa9b0c1b4372a
SaniLubary <santiago.lp.cop@gmail.com>
2021-05-11 18:28:14 -0300

    boards functionality lifted up to game

--- src/component/Board.tsx
+++ src/component/Board.tsx
@@ -1,95 +1,28 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement } from 'react';
 import PropTypes, { InferProps } from "prop-types";
 import Square from './Square';
 
 Board.propTypes = {
-  squares: PropTypes.arrayOf(PropTypes.oneOfType([
-    PropTypes.string.isRequired,
-    PropTypes.oneOf([null]).isRequired
-  ])).isRequired,
+  squares: PropTypes.arrayOf(
+    PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.oneOf([null])
+    ])
+  ).isRequired,
 
-  crossTurn: PropTypes.bool.isRequired,
-  
-  winner: PropTypes.oneOfType([
-    PropTypes.oneOf(['X', 'O'])
-  ])
+  onClick: PropTypes.any.isRequired
 }
 
-Board.defaultProps = {
-  squares: Array(9).fill(null),
-  crossTurn: true
-}
-
-function Board({ squares, crossTurn, winner }: InferProps<typeof Board.propTypes> ): ReactElement
+function Board({ squares, onClick }: InferProps<typeof Board.propTypes> ): ReactElement
 {
-  const [state_squares, setSquares] = useState(squares),
-        [state_crossTurn, setCrossTurn] = useState(crossTurn),
-        [state_winner, setWinner] = useState(winner);
-
   const renderSquare = (i: number) => {
     return (
-      <Square value={state_squares[i]} onClick={() => handleSquareClick(i)} />
+      <Square value={squares[i]} onClick={ () => onClick(i) } />
     ); 
   }
 
-  /**
-   * @param i Square's id
-   */
-  const handleSquareClick = (i: number) => {
-    const squaresArr = state_squares.slice();
-    
-    if (squaresArr[i] == null) {
-      squaresArr[i] = state_crossTurn?'X':'O';
-      setSquares(squaresArr);
-      setCrossTurn(!state_crossTurn);
-    }
-    
-    const winner = checkWinner(squaresArr)
-    if (winner) {
-      setSquares(Array(9).fill(null));
-      setCrossTurn(true);
-      setWinner(winner);
-    }
-  }
-
-  function checkWinner(squaresArr: Array<string|null|undefined>) {
-    let winner = null;
-    const line1 = squaresArr.slice(0,3),
-          line2 = squaresArr.slice(3,6),
-          line3 = squaresArr.slice(6),
-          allLines = [line1, line2, line3];
-
-    // horizontal line win case
-    const checkLine = (line: Array<string|null|undefined>) => line.every( (state_square) => line[0] && line[0] === state_square );
-    for(const line of allLines) {
-      if(checkLine(line)) {
-        console.log('horizontal')
-        return winner = line[0];
-      }
-    }
-
-    // vertical line win case
-    for (const i in line1) {
-      if (line1[i] && (line1[i] === line2[i] && line1[i] === line3[i])) {
-        console.log('vertical')
-        return winner = line1[i];
-      }
-    }
-
-    // diagonal line win case
-    const diagonal_extremes = (line1[0] === line3[2])? line1[0] : ( (line1[2] === line3[0])? line1[2]:null );
-    if (line2[1] && (line2[1] === (diagonal_extremes))) {
-      console.log('diagonal')
-      return winner = line2[1];
-    }
-    
-    return winner;
-  }
-
-
   return (
     <div>
-      <div className="status">Next player: {state_crossTurn?'X':'O'}</div>
       <div className="board">
         <div className="board-row">
           {renderSquare(0)}
--- src/component/Game.tsx
+++ src/component/Game.tsx
@@ -1,36 +1,119 @@
-import React, { ReactElement, useState } from 'react';
-import PropTypes, { InferProps } from "prop-types";
+// eslint-disable-next-line
+import React, { ReactElement, useEffect, useState } from 'react';
+// eslint-disable-next-line
+import PropTypes, { arrayOf, InferProps } from "prop-types";
 import Board from './Board';
+import Button from 'react-bootstrap/Button';
 
-Game.propTypes = {
-  history: PropTypes.arrayOf(
-    PropTypes.arrayOf(PropTypes.oneOfType([
-      PropTypes.string.isRequired,
-      PropTypes.oneOf([null]).isRequired
-    ])
-  )).isRequired,
-
+Game.propTypes = { 
+  history:PropTypes.arrayOf(
+    PropTypes.shape({
+      squares: PropTypes.arrayOf(
+        PropTypes.oneOfType([
+          PropTypes.string,
+          PropTypes.oneOf([null])
+        ])
+      ).isRequired
+    }).isRequired
+  ).isRequired,
+  
   crossTurn: PropTypes.bool.isRequired,
 }
 
 Game.defaultProps = {
-  history: Array(9).fill(null),
+  history: [{squares: Array(9).fill(null)}],
   crossTurn: true
 }
 
 function Game({history, crossTurn}: InferProps<typeof Game.propTypes> ): ReactElement
 {
   const [state_history, setHistory] = useState(history),
-        [state_crossTurn, setCrossTurn] = useState(crossTurn);
+        [state_crossTurn, setCrossTurn] = useState(crossTurn),
+        current = state_history[state_history.length -1],
+        winner = checkWinner(current.squares);
+        
+  function checkWinner(squaresArr: Array<string|null|undefined>) {
+    let winner = null;
+    const line1 = squaresArr.slice(0,3),
+          line2 = squaresArr.slice(3,6),
+          line3 = squaresArr.slice(6),
+          allLines = [line1, line2, line3];
+
+    // horizontal line win case
+    const checkLine = (line: Array<string|null|undefined>) => line.every( (state_square) => line[0] && line[0] === state_square );
+    for(const line of allLines) {
+      if(checkLine(line)) {
+        return winner = line[0];
+      }
+    }
+
+    // vertical line win case
+    for (const i in line1) {
+      if (line1[i] && (line1[i] === line2[i] && line1[i] === line3[i])) {
+        return winner = line1[i];
+      }
+    }
+
+    // diagonal line win case
+    const diagonal_extremes = (line1[0] === line3[2])? line1[0] : ( (line1[2] === line3[0])? line1[2]:null );
+    if (line2[1] && (line2[1] === (diagonal_extremes))) {
+      return winner = line2[1];
+    }
+    
+    return winner;
+  }
+        
+  /**
+   * Method for Square onclick
+   * @param i Square's id, is set by Board's renderSquare(i)
+   */
+  const handleSquareClick = (i: number) => {
+    let history = state_history;
+    const current = state_history[state_history.length -1];
+    const squares = current.squares.slice();
+    
+    if (checkWinner(squares) || squares[i]) {
+      return;
+    }
+    
+    squares[i] = state_crossTurn?'X':'O';
+    history = history.concat([{squares: squares}])
+    setHistory(history);
+    setCrossTurn(!state_crossTurn);
+
+  }
+
+  const handleTime = (i: number) => {
+    console.log('wassup', i);
+  }
+
+  const handleRestart = () => {
+    console.log('way sup');
+  }
+  
+
+  const status = winner? `Winner: ${winner}`:`Next Player: ${state_crossTurn?'X':'O'}`;
   
   return (
     <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+            squares={current.squares}
+            onClick={(i: number) => handleSquareClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>
+            <li className='btn'>
+              <Button variant='info' onClick={ () => handleTime(-1) }>last move</Button>
+            </li>
+            <li className='btn'>
+              <Button variant='info' onClick={ () => handleTime(1) }>next move</Button>
+            </li>
+          </ol>
+
+          <Button variant='warning' onClick={ () => handleRestart() }>Restart</Button>
         </div>
       </div>
   )
